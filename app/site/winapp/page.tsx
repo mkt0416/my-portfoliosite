fcd013b0d39a0697f8bde28cd7a363ada8fe373a
mkt0416 <mktyk7019@gmail.com>
2025-07-18 16:30:04 +0900

    feat winApp AuthGuard

--- app/site/winapp/page.tsx
+++ app/site/winapp/page.tsx
@@ -1,15 +1,16 @@
 
+import AuthGuard from "@/app/components/common/AuthGuard";
 import PageHero from "@/app/components/common/PageHero";
 import WinAppTable from "@/app/components/winapp/WinAppTableList";
 
 const Page = () => {
     return (
-        <>
+        <AuthGuard>
             <PageHero image="/images/computer.svg" title="Windows App" />
             <div className="mt-20 mb-20">
                 <WinAppTable />
             </div>
-        </>
+        </AuthGuard>
     );
 };
 
